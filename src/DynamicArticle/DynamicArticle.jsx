6dde70bc0350b73e4441e9158ad9c77c083c75ef
unknown <mani.nakawe@gmail.com>
2021-03-03 21:57:19 -0800

    react app assignment

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -1,25 +1,23 @@
-import { add } from "lodash";
+  
 import React from "react";
 import HTMLText from "../HTMLText/HTMLText";
 
 const DynamicArticle = (props) => {
   return (
     <article>
-
-      {/* add extra html with javascript attributes here */}
       <header>
-        <h1>You can take a pie on plane</h1>
+        <h1>{props.article.title}</h1>
         <address>
-          <i>by Cathryn Lindsey(
-            <a href="mailto:cathryn.lindsey@nytimes.com">cathryn.lindsey@nytimes.com</a>)</i>
-<br />
-</address>
-<time dateTime="2018-11-21">November 21st 2018, 2:57 pm</time>
+          by {props.article.author} (
+          <a href={props.article.authorEmail}>{props.article.authorEmail}</a>)
+          <br />
+        </address>
+        <time dateTime={props.article.timeStamp}>{props.article.displayDate}</time>
       </header>
-      
       <HTMLText text={props.article.text} />
     </article>
   );
 };
 
 export default DynamicArticle;
+
